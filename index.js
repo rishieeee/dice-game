bf5c28206a56cea7add65b3286aa75852d3658a8
rishieeee <102851742+rishieeee@users.noreply.github.com>
2022-11-24 15:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var randomNO1=Math.floor(Math.random()*6)+1;
 var randomImage="dice" + randomNO1 +".png";
-var randomimgsrc=randomImage;
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomimgsrc);
+image1.setAttribute("src",randomImage);
 var randomNO2=Math.floor(Math.random()*6)+1;
 var randomImage2="dice" + randomNO2 +".png";
-var randomimgsrc2=randomImage2;
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomimgsrc2);
+image2.setAttribute("src",randomImage2);
 if (randomNO1>randomNO2){
   document.querySelector("h1").innerHTML="Player 1 won";
 }
