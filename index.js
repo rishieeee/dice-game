7aa46e0cab7309eab6f8877ddbf719f8fa61d3ca
rishieeee <102851742+rishieeee@users.noreply.github.com>
2022-11-24 15:30:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNO1=Math.floor(Math.random()*6)+1;
 var randomImage="dice" + randomNO1 +".png";
-var randomimgsrc="images/" + randomImage;
+var randomimgsrc=randomImage;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomimgsrc);
 var randomNO2=Math.floor(Math.random()*6)+1;
 var randomImage2="dice" + randomNO2 +".png";
-var randomimgsrc2="images/" + randomImage2;
+var randomimgsrc2=randomImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomimgsrc2);
 if (randomNO1>randomNO2){
